5bd5151f188a853c0823ae022b1816a33363afb8
Rafael Spindola <rafa.spin95@gmail.com>
2022-08-23 18:39:47 -0300

    Minor ChangesLeapYearCalculator Changes

--- LeapYearCalculator/src/LeapYearCalculator.java
+++ LeapYearCalculator/src/LeapYearCalculator.java
@@ -1,11 +1,15 @@
 public class LeapYearCalculator {
     public static boolean isLeapYear(int year) {
-        if (year >= 1 && year <= 9999) {
-            if (year % 4 == 0) {
-                if (year % 100 == 0) {
-                    return (year % 400 == 0);
-                    } else return true;
-                } else return false;
-            } return false;
+        if (year < 1 || year > 9999) {
+            return false;
+        } else if ((year % 4 ==0) &&
+                (year % 100 == 0) &&
+                (year % 400 == 0)) {
+            return true;
+        } else if ((year % 4 == 0) && (year % 100 != 0) ||
+                (year % 100 == 0) && (year % 400 == 0)) {
+            return true;
+        }
+        return false;
         }
     }
